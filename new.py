87ad04f0e1963acb2808a29900c647b983418a71
bhaskardutta95 <bhaskardutta2795@gmail.com>
2025-09-01 19:30:40 +0530

    code refac

--- new.py
+++ new.py
@@ -1,17 +1,17 @@
 import random
 
-def diceRoll():
-    arr = [1,2,3,4,5]
-    ran = random.choice(arr)
+def randomNumber():
+    numbers = [1,2,3,4,5]
+    ran = random.choice(numbers)
     return ran
     
 def getItems(total):
-    arr = []
+    items = []
     for i in range(total):
         item_name = input(f"\nEnter name for item {i+1}: ")
         item_price = int(input(f"Enter price of item {i+1}: "))
-        arr.append((item_name,item_price))
-    return arr
+        items.append((item_name,item_price))
+    return items
 
 def print_bill(customer_name,items):
     print(f"\nHello {customer_name}, here's your bill: ")
@@ -28,7 +28,7 @@ def print_bill(customer_name,items):
 
 def main():
     name = input("Enter your name: ")
-    num = diceRoll()
+    num = randomNumber()
     print(f"Hello {name} you will buy {num} items\n")
     items = getItems(num)
     print_bill(name,items)
