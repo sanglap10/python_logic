6741504e1e76a363dfd78e98bf8d06228136378a
Bhaskar Dutta <36147983+bhaskardutta95@users.noreply.github.com>
2025-09-22 19:52:26 +0530

    Update oop.py

--- oop_concepts/oop.py
+++ oop_concepts/oop.py
@@ -1,1 +1,10 @@
-print("test")
+class Student:
+    def set_name(self,name):
+        self.name = name 
+
+    def get_name(self):
+        return self.name
+
+stu = Student()
+stu.set_name("bhaskar")
+print(stu.get_name())
