b0caa94951f2dc1e5bd510c63a3e88d396c110c9
Bhaskar Dutta <36147983+bhaskardutta95@users.noreply.github.com>
2025-09-26 20:39:10 +0530

    Update abstraction.cs

--- oop_concepts/abstraction.cs
+++ oop_concepts/abstraction.cs
@@ -1,1 +1,61 @@
-Console.WriteLine("This is an example of abstraction.");
+// Example 1:
+
+class UPI : IUPI
+{
+    public bool Pay(float amount)
+    {
+        // logic
+        return true;
+    }
+}
+
+// abstraction
+interface IUPI
+{
+    bool Pay(float amount);
+}
+
+
+class PhonePe
+{
+    public int Payment(float amount)
+    {
+        IUPI upiAbstraction;
+        bool payStatus = upiAbstraction.Pay(amount);
+
+        if (payStatus == true)
+        {
+            Console.WriteLine("Payment Done !");
+        }
+        else
+        {
+            Console.WriteLine("Payment Failed !");
+        }
+    }
+}
+
+// Example 2:
+// abstraction
+interface IUPI
+{
+    void Pay(float amount);
+}
+
+// PhonePe implements UPI
+class PhonePe : IUPI
+{
+    public void Pay(float amount)
+    {
+        Console.WriteLine("Payment processing !!!!");
+    }
+}
+
+class Program
+{
+    public static void main()
+    {
+        IUPI upiAbstraction;
+        upiAbstraction = new PhonePe();
+        upiAbstraction.Pay(500);
+    }
+}
